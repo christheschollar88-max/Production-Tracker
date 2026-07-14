74c718ca2fb7005c483018c6673eb35ac51c9b14
christheschollar88-max <christheschollar88@gmail.com>
2026-07-14 18:35:10 +0200

    Delete Public directory

--- Public/sw.js
+++
@@ -1,5 +0,0 @@
-// This tells the browser that the app has a background worker, allowing it to be installed.
-self.addEventListener('fetch', function(event) {
-    // We are leaving this pass-through for now, but in the future, 
-    // you can use this file to make the app work completely offline!
-});
